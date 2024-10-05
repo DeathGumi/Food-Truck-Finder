2be7690f342f3cc06c4018d6d03d73a98acf56c2
DeathGumi <dueldnh@gmail.com>
2024-10-05 01:35:53 -0700

    had to adjust z-index since map was covering dropdown

--- src/components/ModeSelector.js
+++ src/components/ModeSelector.js
@@ -21,7 +21,7 @@ const ModeSelector = ({ currentMode, onModeChange }) => {
       </button>
 
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5" style={{ zIndex: 200000 }}>
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             <button
               onClick={() => handleModeChange('user')}

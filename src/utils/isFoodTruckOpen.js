52a42f4f9f3257c7019b7c1329361af07709eb94
DeathGumi <dueldnh@gmail.com>
2024-11-02 18:36:19 -0700

    fixed isfoodtruck open some formating split issue

--- src/utils/isFoodTruckOpen.js
+++ src/utils/isFoodTruckOpen.js
@@ -1,23 +1,28 @@
 export function isFoodTruckOpen(hours) {
-    const now = new Date();
-    const currentTime = now.getHours() * 60 + now.getMinutes();
-  
-    const [openStr, closeStr] = hours.split(' - ');
-    const openTime = convertTimeToMinutes(openStr);
-    const closeTime = convertTimeToMinutes(closeStr);
-  
-    return currentTime >= openTime && currentTime < closeTime;
+  // Return false if hours is undefined, null, or not in the correct format
+  if (!hours || typeof hours !== 'string' || !hours.includes(' - ')) {
+      return false;
   }
+
+  const now = new Date();
+  const currentTime = now.getHours() * 60 + now.getMinutes();
+
+  const [openStr, closeStr] = hours.split(' - ');
+  const openTime = convertTimeToMinutes(openStr);
+  const closeTime = convertTimeToMinutes(closeStr);
+
+  return currentTime >= openTime && currentTime < closeTime;
+}
+
+function convertTimeToMinutes(timeStr) {
+  const [time, period] = timeStr.split(' ');
+  let [hours, minutes] = time.split(':').map(Number);
   
-  function convertTimeToMinutes(timeStr) {
-    const [time, period] = timeStr.split(' ');
-    let [hours, minutes] = time.split(':').map(Number);
-    
-    if (period === 'PM' && hours !== 12) {
+  if (period === 'PM' && hours !== 12) {
       hours += 12;
-    } else if (period === 'AM' && hours === 12) {
+  } else if (period === 'AM' && hours === 12) {
       hours = 0;
-    }
-  
-    return hours * 60 + minutes;
   }
+
+  return hours * 60 + minutes;
+}
